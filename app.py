b5678ef5b2a665042047114c45f6c2222f58f27a
Amandeep Singh <deep23910@gmail.com>
2026-07-23 13:57:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -756,4 +756,5 @@ if __name__ == '__main__':
     print("🗄️  Database stored in: ./instance/vulnerable_lab.db")
     print("=" * 70)
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False) # debug must be False
